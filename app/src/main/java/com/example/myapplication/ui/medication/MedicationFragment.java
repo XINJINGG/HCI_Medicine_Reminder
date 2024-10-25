383d838bfde89fd1c8b9ed0306e33f7f65fce1c6
DreamlessOo <D3nnisng1999@gmail.com>
2024-10-25 21:52:02 +0800

    Edited Home page (incomplete)

--- app/src/main/java/com/example/myapplication/ui/medication/MedicationFragment.java
+++ app/src/main/java/com/example/myapplication/ui/medication/MedicationFragment.java
@@ -24,7 +24,7 @@ public class MedicationFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
+        final TextView textView = binding.greetingBanner;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
