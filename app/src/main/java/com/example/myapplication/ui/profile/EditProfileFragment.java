e1fc8b096918740d06c5782a7491a03d9acafd61
Brendan <2301782@sit.singaporetech.edu.sg>
2024-10-28 18:54:13 +0800

    Updated profile and rewards

--- app/src/main/java/com/example/myapplication/ui/profile/EditProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ui/profile/EditProfileFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -39,6 +40,7 @@ public class EditProfileFragment extends Fragment {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(getContext(), "Saved!", Toast.LENGTH_SHORT).show();
                 NavHostFragment.findNavController(EditProfileFragment.this)
                         .navigate(R.id.action_editProfileFragment_to_profileFragment);
             }
